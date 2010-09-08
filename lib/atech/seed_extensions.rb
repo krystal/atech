a02fb9e87ee41e67174a9ae64de737a5eb18ac33
Adam Cooke <adam@atechmedia.com>
2010-09-08 11:11:32 +0100

    add seed extensions

---
+++ lib/atech/seed_extensions.rb
@@ -0,0 +1,27 @@
+module ActiveRecord
+  class Base
+    def save(options = {})
+      return super(options) unless Rails.env.development?
+      
+      unless super(options)
+        puts "\e[31m#{self.inspect}"
+        puts "#{self.errors.inspect}\e[0m"
+        Process.exit(1)
+      else
+        puts "\e[32mcreated\e[0m #{self.class.name} ##{self.id}"
+      end
+    end
+    
+    def self.seedable?
+      actual_count = self.count.to_i
+      return (actual_count == 0) unless Rails.env.development?
+      if actual_count == 0
+        puts "\e[33;44madding #{self.name.to_s} objects\e[0m"
+        true
+      else
+        puts "\e[33mskipping #{self.name.to_s} (#{actual_count} objects already exist)\e[0m"
+        false
+      end
+    end
+  end
+end
