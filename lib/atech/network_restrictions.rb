6ec0cb883e903d0f17371562bd8f3ffccf416207
Adam Cooke <adam@atechmedia.com>
2013-06-17 00:20:44 +0100

    the vpns have moved again

--- lib/atech/network_restrictions.rb
+++ lib/atech/network_restrictions.rb
@@ -30,8 +30,7 @@ module Atech
           '127.0.0.1/32',         # localhost
           '185.22.208.0/25',      # infra
           '109.104.109.0/26',     # bournemouth network
-          '10.17.0.0/16',         # vpn
-          '10.0.2.0/24',          # office
+          '10.0.0.0/16',          # vpn nets
           '2a00:67a0:a:1::/64',   # infra ipv6
           '2a00:67a0:a:5::/64',   # vpn ipv6
           '2a00:67a0:a:6::/64',   # office ipv6
